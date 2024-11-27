912b9a024994497d1df6385ae8339b2bb67c6d70
LuisLiriano03 <llirianomendez@hotmail.com>
2024-11-27 04:52:42 +0100

    Added generic repository interface for reusable CRUD operations.

---
+++ EasyDriveRentals.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace EasyDriveRentals.Domain.Interfaces
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        Task<TModel> GetByIdAsync(int id);
+        Task<TModel> GetEverythingAsync(Expression<Func<TModel, bool>> filter);
+        Task<TModel> CreateAsync(TModel model);
+        Task<bool> UpdateAsync(TModel model);
+        Task<bool> DeleteAsync(TModel model);
+        Task<IQueryable<TModel>> VerifyDataExistenceAsync(Expression<Func<TModel, bool>> filter = null);
+
+    }
+
+}
